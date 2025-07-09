687366dc9bb934a7463193943a44d929e91bedab
Nylah B <nylahbelk3@gmail.com>
2025-07-08 23:55:11 -0400

    Update Playlist.vue
    Deleted w2e album and centered page logo

--- maddys-space/src/views/PlaylistView.vue
+++ maddys-space/src/views/PlaylistView.vue
@@ -9,20 +9,11 @@ import w2e1 from '@/components/icons/w2e1.vue';
     <dancingcat class="cat" />
     <Playlist />
   </main>
-  <div class="wrapper">
-        <w2e1 class="w2e1"/>
-    </div>
 </template>
 
 <style>
 .cat{
     width: 200px;
     display: flex;
-    position: fixed;
-    top: 0px;
-}
-
-.w2e1{
-    width: 150px;
 }
 </style>
