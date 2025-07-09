c370dc4f3514d5ad49c8bd58a3802b8eae108619
Nylah B <nylahbelk3@gmail.com>
2025-07-09 16:11:14 -0400

    Update to Home.vue
    Added "welcome" text.

--- maddys-space/src/views/HomeView.vue
+++ maddys-space/src/views/HomeView.vue
@@ -4,13 +4,31 @@ import greenstar from '@/components/icons/greenstar.vue';
 </script>
 
 <template>
-  <main>
+  <main class="wrapper">
+    <h1 class="text">Welcome</h1>
     <greenstar class="star" />
+    <h1 class="text">Madison <3</h1>
     <Home />
   </main>
 </template>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=DynaPuff:wght@400..700&display=swap');
+
+.wrapper{
+  display: flex;
+  flex-direction: row;
+}
+.text{
+  font-family: "DynaPuff", sans-serif;
+  color: black;
+  display: flex;
+  position: relative;
+  font-weight: 600;
+  font-style: italic;
+  font-size: 50px;
+  text-shadow: -4px 2px 0px silver;
+}
 .star{
   width: 175px;
 }
