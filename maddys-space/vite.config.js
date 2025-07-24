5c555f1beb333dcacf16b014ebec8c58a396ec27
Nylah B <nylahbelk3@gmail.com>
2025-07-24 12:26:43 -0400

    Update vite.config.js

--- maddys-space/vite.config.js
+++ maddys-space/vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/mlc/",
+  base: "../mlc/maddys-space",
   plugins: [
     vue(),
     vueDevTools(),
