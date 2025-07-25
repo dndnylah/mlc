248430a7895f18718a4250cd7a5a2977205fe259
Nylah B <nylahbelk3@gmail.com>
2025-07-25 10:14:08 -0400

    Update

--- maddys-space/vite.config.js
+++ maddys-space/vite.config.js
@@ -1,5 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
@@ -10,6 +10,7 @@ export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    react(),
   ],
   resolve: {
     alias: {
